c6d8c8cfd66f129d7b4465391d57f8d4fb95d07a
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-18 21:44:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstChar(text) {
   // your code here
-}
+	
+	for(let index in text){
+			let asciichar=text.charCodeAt(index);
+			if(asciichar>=65 || asciichar <=90 || asciichar >=97 || asciichar <=122)
+			{
+				let word=String.fromCharCode(asciichar);
+				return word;
+			}
+		}
+	return '';
+		}
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
